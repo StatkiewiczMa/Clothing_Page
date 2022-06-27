2f667b57706e069f9d154946dd10e80a1e8dbd13
Keitar6 <236879@student.pwr.edu.pl>
2022-06-27 09:52:59 +0200

    favicon change

--- src/index.js
+++ src/index.js
@@ -9,20 +9,9 @@ const root = createRoot(container);
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			{/* <header>
-      <Link to='/'>
-				<img />
-			</Link>
-
-			<nav></nav>
-    </header> */}
-
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
